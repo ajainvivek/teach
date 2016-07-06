7f19be4c30aab9e4e3faffd14856125685797e0d
Ajain Vivek <ajainvivek07@gmail.com>
2016-07-06 22:45:40 +0530

    Renamed teach-title to teach-card

--- tests/integration/pods/components/teach-card/component-test.js
+++ tests/integration/pods/components/teach-card/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('teach-title', 'Integration | Component | teach title', {
+moduleForComponent('teach-card', 'Integration | Component | teach card', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{teach-title}}`);
+  this.render(hbs`{{teach-card}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#teach-title}}
+    {{#teach-card}}
       template block text
-    {{/teach-title}}
+    {{/teach-card}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
