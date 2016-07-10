118dda24f17ba9108d3bde50603e3a83105162bb
Ajain Vivek <ajainvivek07@gmail.com>
2016-07-10 18:07:38 +0530

    Landing + Join Page Layout + Design + Route Handling

--- app/pods/components/teach-card/component.js
+++ app/pods/components/teach-card/component.js
@@ -5,8 +5,15 @@ const {
 } = Ember;
 
 export default Ember.Component.extend({
-
   didInsertElement : function () {
       $('.owl-carousel').owlCarousel();
+  },
+  actions: {
+    present () {
+      this.sendAction('present');
+    },
+    view () {
+      this.sendAction('view');
+    }
   }
 });
--- app/pods/landing/controller.js
+++ app/pods/landing/controller.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    hostPresentation () {
+      this.transitionTo('join');
+    },
+    goToSlide () {
+      this.transitionTo('slide');
+    }
+  }
 });
