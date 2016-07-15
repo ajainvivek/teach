a2caacedf828b80575050cdc47096646eb2561bd
Ajain Vivek <ajainvivek07@gmail.com>
2016-07-15 09:10:46 +0530

    Updated Landing Page

--- app/pods/components/teach-card/component.js
+++ app/pods/components/teach-card/component.js
@@ -5,23 +5,8 @@ const {
 } = Ember;
 
 export default Ember.Component.extend({
-  data : [{
-    "id": 1,
-    "slideTemplate": "url",
-    "title": "ES6 - Zombie Attack - Eps 1",
-    "desc": "",
-    "snapshotImg": "http://d1zlh37f1ep3tj.cloudfront.net/wp/wblob/54592E651337D2/22DA/3E75E5/_cCyPoB9Eb1_JTP8ZL_ZuA/EmilysQuotes.Com-Intelligence-teach-learning-Benjamin-Franklin.jpg",
-    "theme": "default",
-    "author": "Ajain Vivek"
-  }, {
-    "id": 2,
-    "slideTemplate": "url",
-    "title": "ES6 - Zombie Attack - Eps 2",
-    "desc": "",
-    "snapshotImg": "http://d1zlh37f1ep3tj.cloudfront.net/wp/wblob/54592E651337D2/22DA/3E75E5/_cCyPoB9Eb1_JTP8ZL_ZuA/EmilysQuotes.Com-Intelligence-teach-learning-Benjamin-Franklin.jpg",
-    "theme": "default",
-    "author": "Ajain Vivek"
-  }],
+  data : [],
+  category : "",
   didInsertElement : function () {
       $('.owl-carousel').owlCarousel({
         autoWidth : true
--- app/pods/landing/controller.js
+++ app/pods/landing/controller.js
@@ -1,6 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  featuredSlides: [],
+  liveSlides: [],
+  isLiveSlidesFilled: function () {
+    let liveSlides = this.get('liveSlides');
+    return (liveSlides.length > 0) ? true : false;
+  }.property('liveSlides'),
+  isFeaturedSlidesFilled: function () {
+    let featuredSlides = this.get('featuredSlides');
+    return (featuredSlides.length > 0) ? true : false;
+  }.property('liveSlides'),
+  loadData: function () {
+    let featuredSlides = [{
+      "id": 1,
+      "slideTemplate": "url",
+      "title": "ES6 - Zombie Attack - Eps 1",
+      "desc": "",
+      "snapshotImg": "/images/es6-ep1.jpg",
+      "theme": "default",
+      "author": "Ajain Vivek"
+    }];
+
+    let liveSlides = [{
+      "id": 2,
+      "slideTemplate": "url",
+      "title": "ES6 - Zombie Attack - Eps 2",
+      "desc": "",
+      "snapshotImg": "/images/es6-ep2.jpg",
+      "theme": "default",
+      "author": "Ajain Vivek"
+    }];
+
+    this.set('liveSlides', liveSlides);
+    this.set('featuredSlides', featuredSlides);
+  }.on('init'),
   actions: {
     joinPresentation () {
       this.transitionTo('join', {
