be5c030a746453fb3c63683555cd09225dfa1fe6
Ajain Vivek <ajainvivek07@gmail.com>
2016-07-16 09:45:34 +0530

    Updated prsentations

--- app/pods/landing/controller.js
+++ app/pods/landing/controller.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import _collection from 'lodash/collection';
 
 export default Ember.Controller.extend({
   featuredSlides: [],
@@ -12,25 +13,30 @@ export default Ember.Controller.extend({
     return (featuredSlides.length > 0) ? true : false;
   }.property('liveSlides'),
   loadData: function () {
-    let featuredSlides = [{
+    let presentations = [{
       "id": 1,
       "slideTemplate": "url",
       "title": "ES6 - Zombie Attack - Eps 1",
       "desc": "",
       "snapshotImg": "/images/es6-ep1.jpg",
       "theme": "default",
-      "author": "Ajain Vivek"
-    }];
-
-    let liveSlides = [{
+      "author": "Ajain Vivek",
+      "isLive": false
+    }, {
       "id": 2,
       "slideTemplate": "url",
       "title": "ES6 - Zombie Attack - Eps 2",
       "desc": "",
       "snapshotImg": "/images/es6-ep2.jpg",
       "theme": "default",
-      "author": "Ajain Vivek"
+      "author": "Ajain Vivek",
+      "isLive": true
     }];
+
+    let featuredSlides = _collection.filter(presentations, {
+      "isLive" : false
+    });
+    let liveSlides = _collection.filter(presentations, 'isLive');
 
     this.set('liveSlides', liveSlides);
     this.set('featuredSlides', featuredSlides);
